0b24c9a6b31cd10c01a5a00adfd28a7c4476a742
ryanyonata <ryan.yonata@gmail.com>
2016-12-13 17:38:47 +0700

    Add proto file and revise wlist

--- makewordlist.py
+++ makewordlist.py
@@ -10,7 +10,7 @@ def writewordlist(infile,outfile):
 		line = re.sub(r"\S+_\S+\t", "", line)
 		line = line.replace("\t","")
 		line = remwithre(line)
-		chars_to_remove = ['-', '!', '?', ',']
+		chars_to_remove = ['!', '?', ',']
 		rx = '[' + re.escape(''.join(chars_to_remove)) + ']'
 		line = re.sub(rx, '', line)
 		lines = line.split()
@@ -19,25 +19,25 @@ def writewordlist(infile,outfile):
 			outfile.write("\n")
 
 with open('A.txt','w') as outfile:
-	with open('Dataset5/transcript/A-raw.tsv') as infile:
+	with open('../Dataset5/transcript/A-raw.tsv') as infile:
 		writewordlist(infile,outfile)
-	with open('Dataset5/transcript/B-raw.tsv') as infile:
+	with open('../Dataset5/transcript/B-raw.tsv') as infile:
 		writewordlist(infile,outfile)
-	with open('Dataset5/transcript/C-raw.tsv') as infile:
+	with open('../Dataset5/transcript/C-raw.tsv') as infile:
 		writewordlist(infile,outfile)
-	with open('Dataset5/transcript/D-raw.tsv') as infile:
+	with open('../Dataset5/transcript/D-raw.tsv') as infile:
 		writewordlist(infile,outfile)
-	with open('Dataset5/transcript/E-raw.tsv') as infile:
+	with open('../Dataset5/transcript/E-raw.tsv') as infile:
 		writewordlist(infile,outfile)
-	with open('Dataset5/transcript/F-raw.tsv') as infile:
+	with open('../Dataset5/transcript/F-raw.tsv') as infile:
 		writewordlist(infile,outfile)
-	with open('Dataset5/transcript/G-raw.tsv') as infile:
+	with open('../Dataset5/transcript/G-raw.tsv') as infile:
 		writewordlist(infile,outfile)
-	with open('Dataset5/transcript/H-raw.tsv') as infile:
+	with open('../Dataset5/transcript/H-raw.tsv') as infile:
 		writewordlist(infile,outfile)
-	with open('Dataset5/transcript/I-raw.tsv') as infile:
+	with open('../Dataset5/transcript/I-raw.tsv') as infile:
 		writewordlist(infile,outfile)
-	with open('Dataset5/transcript/J-raw.tsv') as infile:
+	with open('../Dataset5/transcript/J-raw.tsv') as infile:
 		writewordlist(infile,outfile)
 
 with open('wlist','w') as outfile:
