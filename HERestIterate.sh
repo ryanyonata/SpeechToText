d97a93db7eeb3f0d56c622fc0c50234836d5baa8
Ahmad N. F <fahmadnaufal@gmail.com>
2016-12-14 16:32:45 +0700

    add hmm 7 iterations

---
+++ HERestIterate.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+HMM0_PATH=$1
+NUM_OF_ITERATIONS=$2
+CONFIG_PATH=$3
+PHONES0_PATH=$4
+TRAINSCP_PATH=$5
+MONOPHONES0_PATH=$6
+
+for (( i = 1; i <= $NUM_OF_ITERATIONS; i++ )); do
+    mkdir hmm$i
+    HERest -C $CONFIG_PATH -I $PHONES0_PATH -t 250.0 150.0 1000.0 -S $TRAINSCP_PATH -H $HMM0_PATH/macros -H $HMM0_PATH/hmmdefs -M hmm$i $MONOPHONES0_PATH
+done
