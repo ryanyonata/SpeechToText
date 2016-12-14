8a700acd396c8993b68d0c27e701118d0f73933e
Ahmad N. F <fahmadnaufal@gmail.com>
2016-12-14 17:23:47 +0700

    fix in hmm

--- HERestIterate.sh
+++ HERestIterate.sh
@@ -8,6 +8,7 @@ TRAINSCP_PATH=$5
 MONOPHONES0_PATH=$6
 
 for (( i = 1; i <= $NUM_OF_ITERATIONS; i++ )); do
+    echo "Iteration: $i"
     mkdir hmm$i
-    HERest -C $CONFIG_PATH -I $PHONES0_PATH -t 250.0 150.0 1000.0 -S $TRAINSCP_PATH -H $HMM0_PATH/macros -H $HMM0_PATH/hmmdefs -M hmm$i $MONOPHONES0_PATH
+    HERest -C $CONFIG_PATH -I $PHONES0_PATH -t 250.0 150.0 1000.0 -S $TRAINSCP_PATH -H hmm$((i-1))/macros -H hmm$((i-1))/hmmdefs -M hmm$i $MONOPHONES0_PATH
 done
