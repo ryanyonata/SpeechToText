b1d5fd1ef2536896579257545c6bf2e71025c0df
Ahmad N. F <fahmadnaufal@gmail.com>
2016-12-14 18:00:14 +0700

    step 6

--- HERestIterate.sh
+++ HERestIterate.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 HMM0_PATH=$1
-NUM_OF_ITERATIONS=$2
-CONFIG_PATH=$3
-PHONES0_PATH=$4
-TRAINSCP_PATH=$5
-MONOPHONES0_PATH=$6
+FIRST_ITERATION=$2
+LAST_ITERATION=$3
+CONFIG_PATH=$4
+PHONES0_PATH=$5
+TRAINSCP_PATH=$6
+MONOPHONES0_PATH=$7
 
-for (( i = 1; i <= $NUM_OF_ITERATIONS; i++ )); do
+for (( i = $FIRST_ITERATION; i <= $LAST_ITERATION; i++ )); do
     echo "Iteration: $i"
     mkdir hmm$i
     HERest -C $CONFIG_PATH -I $PHONES0_PATH -t 250.0 150.0 1000.0 -S $TRAINSCP_PATH -H hmm$((i-1))/macros -H hmm$((i-1))/hmmdefs -M hmm$i $MONOPHONES0_PATH
